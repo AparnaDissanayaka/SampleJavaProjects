fe14574b58ef9026caf2d71a3c441c11366e70cf
Aparna Dissanayaka <AparnaDissanayaka@users.noreply.github.com>
2017-06-24 12:53:56 +0530

    Update Call_Sub_Class.java

--- Call_Sub_Class.java
+++ Call_Sub_Class.java
@@ -2,10 +2,18 @@ public class Call_Sub_Class{
 	
 	public static void main(String ar[])
 	{
+		//call to sub class inside in main class
 		sampaleMethod();
+		sampleMethodReturnInt(5);
 	}
+	//create sub method called sampleMethod()
 	public static void sampaleMethod()
 	{
 		System.out.println("This is the test Method");
+	}
+	//create sub method with return type
+	public static int sampleMethodReturnInt(int i)
+	{
+		System.out.println("The value that pass "+i);
 	}
 }
